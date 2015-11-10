b791de2bf18adafadbdccdfd8248d28e54c38d34
Phil Carbone <phil.carbone@virteom.com>
2015-11-09 22:17:05 -0500

    Initial commit

---
+++ BooleanValueToBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace SumpPal
+{
+    public class BooleanValueToBrushConverter : IValueConverter
+    {
+        #region Public Methods
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var boolValue = false;
+            bool.TryParse(value.ToString(), out boolValue);
+            return new SolidColorBrush(boolValue ? Colors.Green : Colors.Red);
+        }
+
+        #endregion Public Methods
+
+        #region Public Methods
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SumpPal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        #region Private Fields
+
+        private GpioController gpio;
+        private GpioPin pin4;
+
+        #endregion Private Fields
+
+        #region Public Fields
+
+        // Using a DependencyProperty as the backing store for SumpIsOn.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SumpIsOnProperty =
+            DependencyProperty.Register("SumpIsOn", typeof(bool), typeof(MainPage), new PropertyMetadata(0));
+
+        #endregion Public Fields
+
+        #region Public Properties
+
+        public bool SumpIsOn
+        {
+            get
+            {
+                return (bool)GetValue(SumpIsOnProperty);
+            }
+            set
+            {
+                if (gpio != null && pin4 != null)
+                {
+                    pin4.Write(value ? GpioPinValue.Low : GpioPinValue.High);
+                }
+
+                SetValue(SumpIsOnProperty, value);
+            }
+        }
+
+        #endregion Public Properties
+
+        #region Public Constructors
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            gpio = GpioController.GetDefault();
+            pin4 = gpio.OpenPin(4);
+            pin4.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public async Task Execute()
+        {
+            while (true)
+            {
+                SumpIsOn = true;
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                SumpIsOn = false;
+                await Task.Delay(TimeSpan.FromMilliseconds(600));
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private async void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Execute();
+        }
+
+        #endregion Private Methods
+    }
+}
